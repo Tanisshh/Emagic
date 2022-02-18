57b35d423ca6ac3d146154734e9ab6ff164e48cc
Tanisshh <tanjilislam288@gmail.com>
2022-02-19 00:41:03 +0600

    store initiating

---
+++ store/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from category.models import Category
+# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=250, blank=True)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    images = models.ImageField(upload_to='photos/products')
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
