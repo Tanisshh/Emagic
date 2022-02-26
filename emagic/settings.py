561066d75069bd3d4a6424111408764112928b48
Tanisshh <tanjilislam288@gmail.com>
2022-02-27 01:34:46 +0600

    initiating cart

--- emagic/settings.py
+++ emagic/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'category',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
--- store/views.py
+++ store/views.py
@@ -20,3 +20,14 @@ def store(request, category_slug=None):
                 'products_count': products_count,
                 }
     return render(request, 'store/store.html', context)
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+
+    context = {
+            'single_product': single_product,
+    }
+    return render(request, 'store/product_detail.html', context)
