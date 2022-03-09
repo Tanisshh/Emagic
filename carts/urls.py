424f6dcfac6a0cdb122a51900c5a44d0edd5ab78
Tanisshh <tanjilislam288@gmail.com>
2022-03-10 00:57:33 +0600

    cart

--- carts/urls.py
+++ carts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.cart, name='cart'),
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart')
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Cart, CartItem
+from store.models import Product
+from store import models
+from django.http import HttpResponse
 
 # Create your views here.
-def cart(request):
-    return render(request, 'store/cart.html')
+
+
+def _cart_id(request):
+    cart = request.session.session_key
+
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+        )
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product = product,
+            quantity = 1,
+            cart = cart,
+        )
+        cart_item.save()
+    return redirect('cart')
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except DoesNotExist:
+            pass
+
+    context ={
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+    }
+
+    return render(request, 'store/cart.html', context)
