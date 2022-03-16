43ee1889102fdb29a5c8aa5ac5e566e0a2683073
Tanisshh <tanjilislam288@gmail.com>
2022-03-16 14:02:28 +0600

    paginator and search

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,7 @@ from .models import Cart, CartItem
 from store.models import Product
 from store import models
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
@@ -60,6 +61,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -68,7 +71,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
 
         tax= (0*total)/100
         grand_total = total+tax
-    except DoesNotExist:
+    except ObjectDoesNotExist:
             pass
 
     context ={
