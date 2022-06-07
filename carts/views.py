53647a8f0476cbf12261a22ebd5279a1a413bf1c
Tanisshh <tanjilislam288@gmail.com>
2022-06-07 23:40:55 +0600

    Checkout

--- carts/views.py
+++ carts/views.py
@@ -4,6 +4,7 @@ from store.models import Product, Variation
 from store import models
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -120,3 +121,31 @@ def cart(request, total=0, quantity=0, cart_items=None):
     }
 
     return render(request, 'store/cart.html', context)
+
+
+
+@login_required(login_url = 'login')
+def checkout(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+
+        tax= (0*total)/100
+        grand_total = total+tax
+    except ObjectDoesNotExist:
+            pass
+
+    context ={
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax':tax,
+        'grand_total':grand_total
+    }
+
+    return render(request, 'store/checkout.html', context)
